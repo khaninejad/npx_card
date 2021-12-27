91f44f89a4fd01382a1a85933f30f42ef88a6496
Payam Khaninejad <khaninejad@gmail.com>
2021-12-27 13:58:20 +0330

    update card info

--- card.js
+++ card.js
@@ -12,9 +12,99 @@ const request = require('request');
 const path = require('path');
 const ora = require('ora');
 const cliSpinners = require('cli-spinners');
+const { Observable } = require('rxjs');
+var ui = new inquirer.ui.BottomBar();
+const https = require('https')
 clear();
+require('dotenv').config()
 
 const prompt = inquirer.createPromptModule();
+let userInfoDetail = "";
+const observe = Observable.create((obs) => {
+    obs.next({
+      type: 'input',
+      name: 'first_name',
+      message: "What's your first name",
+      default() {
+        return 'john';
+      },
+    });
+  
+    obs.next({
+      type: 'input',
+      name: 'last_name',
+      message: "What's your last name",
+      default() {
+        return 'Doe';
+      },
+    });
+  
+    obs.next({
+      type: 'input',
+      name: 'company',
+      message: "What's your company name",
+      default() {
+        return 'Google';
+      },
+    });
+  
+    obs.next({
+      type: 'input',
+      name: 'position',
+      message: "What's your current position in your current company ",
+      default() {
+        return 'HR';
+      },
+    });
+  
+    obs.next({
+      type: 'input',
+      name: 'email',
+      message: "What's your email",
+      validate(value) {
+        const pass = value.match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+        if (pass) {
+          return true;
+        }
+  
+        return 'Please enter a valid email';
+      },
+    });
+    obs.complete();
+  });
+
+  const output = [];
+  const botQuestions = [
+    {
+      type: 'input',
+      name: 'askQuestion',
+      message: "please type your question",
+      validate: async (answer) => {
+        ui.log.write('typing.');
+        await askQuestion("payam",answer);
+        return true;
+      },
+    },
+    {
+      type: 'confirm',
+      name: 'askAgain',
+      message: 'If you have another question, ask again',
+      default: true,
+    },
+  ];
+  
+  function ask(userinfos) {
+    inquirer.prompt(botQuestions).then((answers) => {
+      output.push(answers.askQuestion);
+      if (answers.askAgain) {
+        ask();
+      } else {
+        console.log('Thank you for connecting. please feel free to contact me');
+      }
+    });
+  }
 
 const questions = [
     {
@@ -27,6 +117,25 @@ const questions = [
                 value: () => {
                     open("mailto:khaninejad@gmail.com");
                     console.log("\nDone, see you soon at inbox.\n");
+                }
+            },
+            {
+                name: `offline ${chalk.yellowBright.bold("Interview")}?`,
+                value: () => {
+                    console.log("\nLet's meet.\n");
+                    inquirer.prompt(observe).then((answers) => {
+                        if (answers.position.includes('developer')){
+                            console.log('Hello dude,');
+                          }else if (answers.position.includes('developer')){
+                            console.log('Hello future boss :D' );
+                          }else{
+                            console.log('Hello '+ answers.first_name + ' ' + answers.last_name + ', ' );
+                          }
+                          console.log("\nNow you're going to connect with my assistant, feel free to ask anything you like.\n");
+                        userInfoDetail = answers;
+                        ask();
+                      });
+                      
                 }
             },
             {
@@ -46,6 +155,7 @@ const questions = [
                     });
                 }
             },
+            
             {
                 name: "Just quit.",
                 value: () => {
@@ -119,3 +229,34 @@ const tip = [
 console.log(tip);
 
 prompt(questions).then(answer => answer.action());
+async function askQuestion(userInfo, message){
+    return new Promise(function(resolve, reject) {
+    var request = require('request');
+    var options = {
+      'method': 'POST',
+      'url': process.env.chatbotURL,
+      'headers': {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "message": message,
+        "username": userInfoDetail.email,
+        "first_name":userInfoDetail.first_name,
+        "last_name": userInfoDetail.last_name,
+        "position": userInfoDetail.position,
+        "company": userInfoDetail.company
+      })
+    
+    };
+    request(options, function (error, response) {
+      if (error){
+        reject();
+        throw new Error(error);
+      } 
+      const obj = JSON.parse(response.body);
+      console.log('\x1b[33m%s\x1b[0m','Payam: '+ obj.text);  //yellow
+      resolve()
+    });
+});
+    
+}
